255af74425deb06aaa2eb7ca270f7d2b3a182f25
lasb0001 <bolajilasisi13@gmail.com>
2026-07-06 17:36:10 +0100

    Finish AdMob Plus setup

--- www/script.js
+++ www/script.js
@@ -256,3 +256,16 @@ window.addEventListener("load", function () {
     }
 
 });
+document.addEventListener("deviceready", function () {
+    console.log("Device ready");
+
+    console.log("Cordova:", typeof cordova);
+    console.log("AdMob:", typeof admob);
+
+    if (typeof admob === "undefined") {
+        alert("AdMob plugin not loaded.");
+        return;
+    }
+
+    alert("AdMob plugin loaded successfully!");
+});
